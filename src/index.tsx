70c02614ff153744a06d277367c66334615917d2
Angelina <angelina@MacBook-Air-Angelina.local>
2023-01-23 23:59:07 +0200

    add props

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 
 function List() {
     return (
-        <React.Fragment>
+        <>
             <ul>
                 <li>List item 1</li>
                 <li>List item 2</li>
@@ -15,18 +15,44 @@ function List() {
                 aperiam, voluptate adipisci consectetur aspernatur placeat earum
                 tempore repellat quia libero odio.
             </p>
-        </React.Fragment>
+        </>
     )
 }
 
-function Title() {
-    return <h1>Hello TS</h1>
+type TitleProps = {
+    name: string
+    text: string
+    year: number
+}
+
+const Title = ({ name, text, year }: TitleProps) => {
+    return (
+        <>
+            <h1>Hello {name}</h1>
+            <p>{text}</p>
+            <div>Year: {year}</div>
+        </>
+    )
 }
 
 const App = () => {
     return (
         <div className="app">
-            <Title />
+            <Title
+                name="App"
+                text="Lorem ipsum dolor sit, amet consectetur adipisicing elit."
+                year={2023}
+            />
+            <Title
+                name="React"
+                text="Lorem ipsum dolor sit, amet consectetur adipisicing elit."
+                year={2022}
+            />
+            <Title
+                name="TS"
+                text="Lorem ipsum dolor sit, amet consectetur adipisicing elit."
+                year={2021}
+            />
             <List />
         </div>
     )
