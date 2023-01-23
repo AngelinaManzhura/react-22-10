78e73fb42244f201fb88f4241e3abe1dbb4880f4
Angelina <angelina@MacBook-Air-Angelina.local>
2023-01-23 23:06:12 +0200

    add React.Fragment component

--- src/index.tsx
+++ src/index.tsx
@@ -1,31 +1,40 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-// without jsx
-// const title = React.createElement('h1', { id: 'title' }, 'Hello react.js')
-// console.log(title)
+function List() {
+    return (
+        <React.Fragment>
+            <ul>
+                <li>List item 1</li>
+                <li>List item 2</li>
+                <li>List item 3</li>
+            </ul>
+            <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iusto
+                tenetur nisi quae consequatur magni et, fugit, quisquam nam
+                aperiam, voluptate adipisci consectetur aspernatur placeat earum
+                tempore repellat quia libero odio.
+            </p>
+        </React.Fragment>
+    )
+}
 
-// jsx => js extended
-// tsx => ts extended
-const title = <h1 id="title">Hello react.js</h1>
+function Title() {
+    return <h1>Hello TS</h1>
+}
 
-const content = (
-    <div>
-        {title}
-        {10 + 10}
-        <ul className="list">
-            <li>List item 1</li>
-            <li>List item 2</li>
-            <li>List item 3</li>
-        </ul>
-        <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis
-            soluta minus cum voluptates iure veniam ea repudiandae temporibus,
-            voluptatem perferendis debitis, quos blanditiis incidunt dolore
-            accusantium obcaecati mollitia? Alias, neque.
-        </p>
-    </div>
-)
+const App = () => {
+    return (
+        <div className="app">
+            <Title />
+            <List />
+        </div>
+    )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<React.StrictMode>{content}</React.StrictMode>)
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+)
