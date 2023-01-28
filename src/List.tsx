6fd747d8ed39b505c09186dd929666c512904691
Angelina <angelina@MacBook-Air-Angelina.local>
2023-01-28 23:35:57 +0200

    add style

--- src/List.tsx
+++ src/List.tsx
@@ -1,5 +1,6 @@
 type Props = {}
 export const List = (props: Props) => {
+    let data = false
     return (
         <>
             <ul>
@@ -7,6 +8,18 @@ export const List = (props: Props) => {
                 <li>List 2</li>
                 <li>List 3</li>
             </ul>
+            <p
+                style={{
+                    color: 'white',
+                    backgroundColor: data ? 'purple' : 'tomato',
+                    padding: '15px',
+                }}
+            >
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iusto
+                tenetur nisi quae consequatur magni et, fugit, quisquam nam
+                aperiam, voluptate adipisci consectetur aspernatur placeat earum
+                tempore repellat quia libero odio.
+            </p>
             <p>
                 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iusto
                 tenetur nisi quae consequatur magni et, fugit, quisquam nam
