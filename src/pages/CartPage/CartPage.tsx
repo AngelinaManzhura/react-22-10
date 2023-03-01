b309540f4b530ad888a52bbb78cb039f6a22132d
Angelina <angelina@MacBook-Air-Angelina.local>
2023-03-01 22:58:48 +0200

    add item in CartPage and price for one item

--- src/pages/CartPage/CartPage.tsx
+++ src/pages/CartPage/CartPage.tsx
@@ -1,12 +1,47 @@
 import { Typography } from '@mui/material'
+import productsArray, { getProductsObject, Product } from 'utils/productsArray'
 
-type Props = {}
+type Props = {
+    productsInCart: {
+        [id: number]: number
+    }
+    productsObject?: {
+        [id: number]: Product
+    }
+}
 
-const CartPage = (props: Props) => {
+const CartPage = ({
+    productsInCart,
+    productsObject = getProductsObject(productsArray),
+}: Props) => {
     return (
-        <Typography variant="h4" component="h1">
-            Cart
-        </Typography>
+        <>
+            <Typography variant="h4" component="h1">
+                Cart
+            </Typography>
+            <div>
+                <div>
+                    {Object.keys(productsInCart).map((productId) => (
+                        <div key={productId}>
+                            {productsObject[parseInt(productId)].title}:{' '}
+                            {productsInCart[parseInt(productId)]}
+                            {'  Price for one item: '}
+                            {productsObject[parseInt(productId)].price}
+                        </div>
+                    ))}
+                </div>
+                <div>
+                    Total: ${' '}
+                    {Object.keys(productsInCart).reduce(
+                        (total, productId) =>
+                            total +
+                            productsObject[parseInt(productId)].price *
+                                productsInCart[parseInt(productId)],
+                        0
+                    )}
+                </div>
+            </div>
+        </>
     )
 }
 
