5d045427b351dd0c3d266b98844b4e1c7b307a88
Angelina <angelina@MacBook-Air-Angelina.local>
2023-03-05 00:45:30 +0200

    прокинули пропсы для удаления товаров на странице CartPage

--- src/pages/CartPage/CartPage.tsx
+++ src/pages/CartPage/CartPage.tsx
@@ -7,9 +7,11 @@ type Props = {
     productsInCart: {
         [id: number]: number
     }
+
+    removeProductFromCart: (id: number) => void
 }
 
-const CartPage = ({ productsInCart }: Props) => {
+const CartPage = ({ productsInCart, removeProductFromCart }: Props) => {
     return (
         <div>
             <Typography variant="h4" component="h1">
@@ -19,6 +21,7 @@ const CartPage = ({ productsInCart }: Props) => {
                 <CartProductList
                     productsInCart={productsInCart}
                     CartItem={CartProductListItemExtended}
+                    removeProductFromCart={removeProductFromCart}
                 />
             </Grid>
 
