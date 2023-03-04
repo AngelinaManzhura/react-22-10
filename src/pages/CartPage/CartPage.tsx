1b356270bf454e7a149d0a9ed73b0a665f888c7f
Angelina <angelina@MacBook-Air-Angelina.local>
2023-03-04 18:33:49 +0200

    hw-10 add information in CartPage

--- src/pages/CartPage/CartPage.tsx
+++ src/pages/CartPage/CartPage.tsx
@@ -1,12 +1,43 @@
 import { Typography } from '@mui/material'
+import productsArray, { getProductsObject, Product } from 'utils/productsArray'
 
-type Props = {}
+type Props = {
+    productsInCart: {
+        [id: number]: number
+    }
+    productsObject?: {
+        [id: number]: Product
+    }
+}
 
-const CartPage = (props: Props) => {
+const CartPage = ({
+    productsInCart,
+    productsObject = getProductsObject(productsArray),
+}: Props) => {
     return (
-        <Typography variant="h4" component="h1">
-            Cart
-        </Typography>
+        <div>
+            <Typography variant="h4" component="h1">
+                Cart
+            </Typography>
+            <div>
+                {Object.keys(productsInCart).map((productId) => (
+                    <div key={productId}>
+                        {productsObject[parseInt(productId)].title}:{' '}
+                        {productsInCart[parseInt(productId)]}
+                    </div>
+                ))}
+            </div>
+            <div>
+                Total: ${' '}
+                {Object.keys(productsInCart).reduce(
+                    (total, productId) =>
+                        total +
+                        productsObject[parseInt(productId)].price *
+                            productsInCart[parseInt(productId)],
+                    0
+                )}
+            </div>
+        </div>
     )
 }
 
