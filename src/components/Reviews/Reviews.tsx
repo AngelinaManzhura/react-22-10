a9c66467a03e664c1a6c4e8bc8a6ab6074d49488
Angelina <angelina@MacBook-Air-Angelina.local>
2023-03-07 18:17:50 +0200

    добавили отзывы Reviews на страницу HomePage

---
+++ src/components/Reviews/Reviews.tsx
@@ -0,0 +1,43 @@
+import { Card, CardContent, Typography } from '@mui/material'
+import { divide } from 'lodash'
+import { useState } from 'react'
+
+type Props = {}
+
+type Review = {
+    name: string
+    text: string
+}
+
+const Reviews = (props: Props) => {
+    const arrReviews: Review[] = [
+        {
+            name: 'Христина',
+            text: 'Покупкою задоволена на всі 100!!!! Гарний дизайн, приємний колір, легкий, зручно поміщається в жіночій руці.',
+        },
+        {
+            name: 'Тарас',
+            text: 'Швидка доставка.Телефон Працює бездогано 10/10',
+        },
+    ]
+
+    const [reviews, setReviews] = useState<Review[]>(arrReviews)
+
+    return (
+        <>
+            <Typography component="h2" variant="h4" sx={{ margin: '40px 0' }}>
+                Reviews
+            </Typography>
+            {reviews.map(({ name, text }, i) => (
+                <Card variant="outlined" key={i} sx={{ margin: '30px 0' }}>
+                    <CardContent>
+                        <div>{name}:</div>
+                        <div>{text}</div>
+                    </CardContent>
+                </Card>
+            ))}
+        </>
+    )
+}
+
+export default Reviews
